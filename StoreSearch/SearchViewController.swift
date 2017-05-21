0e370d2e129b560b9c7b6b864104dbb3f2e08389
Benjamin jones <ben@Benjamins-MacBook-Pro.local>
2017-05-20 22:03:33 -0700

    rm xcode specific folders

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -49,9 +49,13 @@ class SearchViewController: UIViewController {
         
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ShowDetail" {
+            let detailViewController = segue.destination as! DetailViewController
+            let indexPath = sender as! IndexPath
+            let searchResult = searchResults[indexPath.row]
+            detailViewController.searchResult = searchResult
+        }
     }
     
     func iTunesURL(searchText: String, category: Int) -> URL {
@@ -243,6 +247,7 @@ extension SearchViewController: UITableViewDataSource {
 extension SearchViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+        performSegue(withIdentifier: "ShowDetail", sender: indexPath)
     }
     
     func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
